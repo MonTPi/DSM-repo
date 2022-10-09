e4e7588a8735b737e00421d17a5966d0a3a12d17
JDL <josh.lilley98@gmail.com>
2022-10-09 19:19:11 +0100

    Mid lecture 3

---
+++ CSP50_Lec2.py
@@ -0,0 +1,11 @@
+#This will have content
+
+# Terminal command shortcuts
+
+#ls #list files in a given folder
+
+#clear # clears the command terminal 
+
+#cp , ... , ... #Copy, file.py , newfile.py
+
+#../folder.py #.. = parent folder
