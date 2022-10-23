9184df27a8ae9f140b98797dae2e8f72e6d6ab4a
JDL <josh.lilley98@gmail.com>
2022-10-23 15:57:39 +0100

    Full lecture 4
    New lecture file for lecture_5

---
+++ CSP50_Lec3.py
@@ -0,0 +1,31 @@
+def el():
+    if A > B:
+        print('A is big')
+    elif A == B :
+        print('ooohhee they bigg!')
+    else :
+         print('B is Big')
+
+
+def BA(a,b) :
+    if B == A:
+        print('B = A')
+    else:
+        print('A != B')
+
+
+def iseven(x) :
+    if x % 2 == 0:
+        print('oh she even')
+    else :
+        print('oh she not even')
+
+X = int(input('what is X?'))
+A = int(input('what is A?'))
+B = int(input('what is B?'))
+
+BA()
+
+el()
+
+iseven()
