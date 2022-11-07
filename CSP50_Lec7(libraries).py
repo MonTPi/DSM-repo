abc93ff28a3d3917e3efa85657beff0ed0f35e37
JDL <josh.lilley98@gmail.com>
2022-11-07 00:22:12 +0000

    Lecture 7
    added names to previous lessons

---
+++ CSP50_Lec7(libraries).py
@@ -0,0 +1,16 @@
+#Code pre written that can be used in your project. Moduels do a single process.
+##can you module rather than copy & paste
+
+#random mods are basic functionality. - doc.python/3/library/python.html
+
+import random
+
+def main():
+ coin = random.choice(seq)
+ print(coin)
+
+
+seq = ["heads","tails"]
+ 
+
+main()
