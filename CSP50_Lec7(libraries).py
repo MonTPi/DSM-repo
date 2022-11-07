d79b7be32328c677ad7bcc8c5d3ed0e41347ef58
JDL <josh.lilley98@gmail.com>
2022-11-07 21:50:05 +0000

    test

--- CSP50_Lec7(libraries).py
+++ CSP50_Lec7(libraries).py
@@ -89,24 +89,16 @@ import sys
 #helps to format json files
 import json
 
-print(response['kind'])
 
 ##exits program if incorrectly formatted
 if len(sys.argv) != 2:
     sys.exit()
 ##attempts to make a request for data on a song, stores as var
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=2&term=" + sys.argv[1])
 print(json.dumps(response.json(), indent = 2))
 
+o = response.json()
 
-
-
-
-
-
-
-
-
-
-
-
+for result in o['results'] :
+    print(result['trackName'])
+    print(result['trackPrice'])
